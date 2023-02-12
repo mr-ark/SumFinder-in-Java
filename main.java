5dfbb4f27f0af9a335504f00af437a16bcd05900
Annuay [Ark] <100431877+mr-ark@users.noreply.github.com>
2023-02-12 08:24:42 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+package //yourprojectname;
+
+import java.util.Scanner;
+
+public class //yourclassname {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+	
+		System.out.println("Enter Numbers:"); 
+		
+		int a=s.nextInt();
+		int b=s.nextInt();
+		
+		int c=a+b;
+		
+		System.out.print(" Result:" +c);
+
+	}
+
+}
